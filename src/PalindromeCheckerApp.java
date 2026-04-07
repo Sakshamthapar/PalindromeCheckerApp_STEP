893e0fc01f25ff8d8461c2ace1cd48bb602390fe
Saksham Thapar <sakshamthapar22@gmail.com>
2026-04-07 10:29:43 +0530

    UC9: Palindrome check using recursion

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,29 @@
-// UC8
-public static void checkPalindromeUsingLinkedList() {
-    String input = "level";
+// UC9
+public static void checkPalindromeUsingRecursion() {
+    String input = "madam";
 
-    // Create LinkedList
-    java.util.LinkedList<Character> list = new java.util.LinkedList<>();
-
-    // Add characters
-    for (char c : input.toCharArray()) {
-        list.add(c);
-    }
-
-    boolean isPalindrome = true;
-
-    // Compare first and last
-    while (list.size() > 1) {
-        if (list.removeFirst() != list.removeLast()) {
-            isPalindrome = false;
-            break;
-        }
-    }
+    boolean isPalindrome = isPalindromeRecursive(input, 0, input.length() - 1);
 
     if (isPalindrome) {
-        System.out.println("UC8 Result: \"" + input + "\" is a Palindrome.");
+        System.out.println("UC9 Result: \"" + input + "\" is a Palindrome.");
     } else {
-        System.out.println("UC8 Result: \"" + input + "\" is NOT a Palindrome.");
+        System.out.println("UC9 Result: \"" + input + "\" is NOT a Palindrome.");
     }
+}
+
+// Recursive function
+public static boolean isPalindromeRecursive(String s, int start, int end) {
+
+    // Base condition
+    if (start >= end) {
+        return true;
+    }
+
+    // Check mismatch
+    if (s.charAt(start) != s.charAt(end)) {
+        return false;
+    }
+
+    // Recursive call
+    return isPalindromeRecursive(s, start + 1, end - 1);
 }
