a6cd1b03d7e75ccf9a6af8a8ffa5f06527378e90
Saksham Thapar <sakshamthapar22@gmail.com>
2026-04-07 09:51:45 +0530

    UC2: Hardcoded palindrome check implemented

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,12 +2,30 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // ===== UC1: Welcome Message =====
         System.out.println("========================================");
         System.out.println(" Welcome to the Palindrome Checker App ");
         System.out.println("========================================");
 
         System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.\n");
 
-        System.out.println("System initialized successfully.");
+
+        // ===== UC2: Hardcoded Palindrome Check =====
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
     }
 }
